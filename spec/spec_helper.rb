5948414f79ed75600a05255c63890a86daafd10a
Emma <emma_williams1@hotmail.co.uk>
2014-05-07 16:26:04 +0100

    database cleaner added with test passing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 require './server' 
+require 'database_cleaner'
 # Remember environment variables from week 1?
 ENV["RACK_ENV"] = 'test' # because we need to know what database to work with
 #here we specify the environemt so that the tests are using the right database
@@ -12,6 +13,30 @@ ENV["RACK_ENV"] = 'test' # because we need to know what database to work with
 # loaded once.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+Rspec.configure do |config|
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do 
+    DatabaseCleaner.start 
+  end
+
+  config.after(:each) do 
+    DatabaseCleaner.clean
+  end
+
+end
+
+
+
+
+
+
+
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
